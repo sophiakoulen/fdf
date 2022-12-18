49057caa56ea1c4149157d32525af5dbd2d03d37
Sophia Mercedes Solange Koulen <skoulen@c1r10s2.42lausanne.ch>
2022-12-18 14:12:13 +0100

    norminetted parsing funcs

--- parsing/parsing.c
+++ parsing/parsing.c
@@ -6,27 +6,23 @@
 /*   By: skoulen <skoulen@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/18 11:20:47 by skoulen           #+#    #+#             */
-/*   Updated: 2022/12/18 13:56:13 by skoulen          ###   ########.fr       */
+/*   Updated: 2022/12/18 14:11:33 by skoulen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	*get_ints(char *line)
+int	*get_ints(char *line, int n)
 {
 	char	**tab;
-	int		*ints;
-	int		n;
+	int		*res;
 	int		i;
 
 	tab = ft_split(line, ' ');
 	if (!tab)
 		return (0);
-	n = 0;
-	while (tab[n])
-		n++;
-	ints = malloc(n * sizeof(*ints));
-	if (!ints)
+	res = malloc(n * sizeof(*res));
+	if (!res)
 	{
 		cleanup_strs(tab);
 		return (0);
@@ -34,11 +30,11 @@ int	*get_ints(char *line)
 	i = 0;
 	while (i < n)
 	{
-		ints[i] = ft_atoi(tab[i]);
+		res[i] = ft_atoi(tab[i]);
 		i++;
 	}
 	cleanup_strs(tab);
-	return (ints);
+	return (res);
 }
 
 /*
@@ -90,12 +86,35 @@ int	get_width(char *filename)
 	return (i);
 }
 
+/*
+	Returns 0 in case of success,
+	-1 in case of failure (malloc failure)
+*/
+int	fill_map(int fd, int **map, int rows, int cols)
+{
+	int		i;
+	char	*line;
+
+	i = 0;
+	while (i < rows)
+	{
+		line = get_next_line(fd);
+		map[i] = get_ints(line, cols);
+		if (!map[i])
+		{
+			cleanup_map(map);
+			return (-1);
+		}
+		free(line);
+		i++;
+	}
+	return (0);
+}
+
 int	**parse_map(char *filename, int *rows, int *cols)
 {
 	int		**map;
 	int		fd;
-	char	*line;
-	int		i;
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
@@ -103,22 +122,9 @@ int	**parse_map(char *filename, int *rows, int *cols)
 	*rows = get_height(filename);
 	*cols = get_width(filename);
 	map = ft_calloc(*rows, sizeof(*map));
-	i = 0;
-	while (1)
-	{
-		line = get_next_line(fd);
-		if (line)
-		{
-			map[i] = get_ints(line);
-			if (!map[i])
-			{
-				cleanup_map(map);
-				return (0);
-			}
-		}
-		if (!line)
-			break ;
-		i++;
-	}
+	if (!map)
+		return (0);
+	fill_map(fd, map, *rows, *cols);
+	close(fd);
 	return (map);
 }
